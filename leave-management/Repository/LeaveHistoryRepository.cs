1f6fb15063d1cab626cf69e9376390ae0363dbd4
Petar Gavrilov <petarmgav@gmail.com>
2021-02-26 12:58:57 +0200

    Add project files.

---
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            return Save();
+        }
+    }
+}
